34a347246eaf75f8f12c84d6cdf7e3a41ccc2248
Asadulla Juraev <tezkormedia@gmail.com>
2021-11-28 22:29:20 +0500

    Unit Tests

--- StoryboardContactTests/StoryboardContactTests.swift
+++ StoryboardContactTests/StoryboardContactTests.swift
@@ -10,24 +10,86 @@ import XCTest
 
 class StoryboardContactTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testContactListApiResponseNotNil(){
+        let ex = expectation(description: "testContactListApiResponseNotNil")
+        
+        AFHttp.get(url: AFHttp.API_CONTACT_LIST, params: AFHttp.paramsEmpty()) { response in
+            switch response.result {
+            case .success:
+                XCTAssertNotNil(response)
+                ex.fulfill()
+            case let .failure(error):
+                XCTAssertNil(error)
+                ex.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 10) { (error) in
+            if let error = error {
+                XCTFail("error: \(error)")
+            }
+        }
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testContactCreateApiResponseNotNil(){
+        let ex = expectation(description: "testContactCreateApiResponseNotNil")
+        let contact = Contact(name: "Jaguar", number: "+998912345678")
+        AFHttp.post(url: AFHttp.API_CONTACT_CREATE, params: AFHttp.paramsContactCreate(contact: contact)) { response in
+            switch response.result {
+            case .success:
+                XCTAssertNotNil(response)
+                ex.fulfill()
+            case let .failure(error):
+                XCTAssertNil(error)
+                ex.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 10) { (error) in
+            if let error = error {
+                XCTFail("error: \(error)")
+            }
         }
     }
 
+    func testContactEditApiResponse(){
+        let ex = expectation(description: "testContactEditApiResponse")
+        let contact = Contact(name: "Jaguary Edit", number: "+998912345687")
+        let id = "2"
+        AFHttp.put(url: AFHttp.API_CONTACT_UPDATE + id, params: AFHttp.paramsContactUpdate(contact: contact)) { response in
+            switch response.result {
+            case .success:
+                XCTAssertNotNil(response)
+                ex.fulfill()
+            case let .failure(error):
+                XCTAssertNil(error)
+                ex.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 10) { (error) in
+            if let error = error {
+                XCTFail("error: \(error)")
+            }
+        }
+    }
+    
+    func testContactDeleteApiResonse(){
+        let ex = expectation(description: "testContactEditApiResponse")
+        let id = "4"
+        AFHttp.del(url: AFHttp.API_CONTACT_DELETE + id, params: AFHttp.paramsEmpty(), handler: { response in
+            switch response.result {
+            case .success:
+                XCTAssertNotNil(response)
+                ex.fulfill()
+            case let .failure(error):
+                XCTAssertNil(error)
+                ex.fulfill()
+            }
+        })
+        
+        waitForExpectations(timeout: 10) { (error) in
+            if let error = error {
+                XCTFail("error: \(error)")
+            }
+        }
+    }
+    
 }
